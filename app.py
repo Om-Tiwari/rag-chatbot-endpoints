0b337ee355ad1ad332a9cc0022f54308894e0547
Om Tiwari['MrSus'] <omthegreat085@gmail.com>
2025-01-28 16:06:12 +0530

    :)

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ def health_check():
         return jsonify(status="unhealthy", error=str(e)), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
